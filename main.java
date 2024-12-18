65712f2c7c0cd6d2e54740faf3201a037e113940
AkilNanda EK <70651752+horridprogrammer@users.noreply.github.com>
2024-12-18 17:11:51 +0530

    Update bugs in main file

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("this is a introduction")
-        System.out.println("Updating the branch")
+        System.out.println("this is a introduction");
+        System.out.println("Updating the branch");
     }
 }
